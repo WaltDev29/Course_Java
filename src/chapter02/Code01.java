bb946fd0073451cba38592b3a91165f4d920c042
WaltDev29 <dwdr29@gmail.com>
2025-03-17 11:52:18 +0900

    main Method 사용법, 연결연산자/덧셈연산자, println/print 차이점 학습

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("AISoftware");   // 자동 줄바꿈
+        System.out.print("김호석\n");        // 특별한 형식없이 출력
+        System.out.printf("%d\n", 1);       // 형식 지정해서 출력
+        System.out.println("result : "+(100+200));     // '+' : 연결 연산자
+        System.out.println(100 + 200 + "won");
+    }
+}
