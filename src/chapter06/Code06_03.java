bccf113beaa16fa5f6e003a89da6cc8c2453509e
WaltDev29 <dwdr29@gmail.com>
2025-05-12 10:34:24 +0900

    6장 반복문 for문. 홀수의 합계 구하기

--- src/chapter06/Code06_03.java
+++ src/chapter06/Code06_03.java
@@ -4,9 +4,13 @@ public class Code06_03 {
     public static void main(String[] args) {
         int hap = 0;
         int n = 100;
+        String oddEven = "";
         for (int i=1; i<=n; i++) {
-            hap += i;
+            if (i % 2 != 0) {
+                oddEven = "홀수";
+                hap += i;
+            }
         }
-        System.out.println("1 ~ " + n + "까지의 합계는 " + hap + "입니다.");
+        System.out.println("1 ~ " + n + "까지의 " + oddEven + "의 합계는 " + hap + "입니다.");
     }
 }
