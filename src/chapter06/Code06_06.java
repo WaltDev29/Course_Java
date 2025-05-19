c428f23ce08d8077ab45908ace25421ef62fa7ed
WaltDev29 <dwdr29@gmail.com>
2025-05-19 09:21:29 +0900

    While문. 반복 횟수 지정

---
+++ src/chapter06/Code06_06.java
@@ -0,0 +1,12 @@
+package chapter06;
+
+public class Code06_06 {
+    public static void main(String[] args) {
+        // for문과 같이 반복 횟수를 정해놓고 사용하는 경우
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i+1 + "번 : 인공지능소프트웨어과");
+            i++;
+        }
+    }
+}
