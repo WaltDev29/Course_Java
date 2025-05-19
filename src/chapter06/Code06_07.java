16e4a572644ddba0127a554b0c19bb1c3cc2e34f
WaltDev29 <dwdr29@gmail.com>
2025-05-19 09:26:45 +0900

    While문. 무한 루프

---
+++ src/chapter06/Code06_07.java
@@ -0,0 +1,10 @@
+package chapter06;
+
+public class Code06_07 {
+    // 무한 루프
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("AI");
+        }
+    }
+}
