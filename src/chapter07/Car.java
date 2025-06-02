8b132af8f069d129aef2fb606866a72aefc7c8a8
WaltDev29 <waltdev29@gmail.com>
2025-06-02 10:52:37 +0900

    7장 클래스와 객체. 필드와 메서드 구현, 서로 다른 객체 사용

---
+++ src/chapter07/Car.java
@@ -0,0 +1,77 @@
+package chapter07;
+
+// Alt + Insert (생성자, getter, setter 자동생성)
+public class Car {
+    private String name;
+    private String product;
+    private int price;
+    private int year;
+    private int cc;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void start() {
+        System.out.println("시동을 겁니다.");
+    }
+    public void drive(int speed) {
+        System.out.println("시속"+ speed +"km로 달린다.");
+    }
+    public void mvF() {
+        System.out.println("전진합니다.");
+    }
+    public void mvB() {
+        System.out.println("후진합니다.");
+    }
+    public void stop() {
+        System.out.println("정지합니다.");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", product='" + product + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", cc=" + cc +
+                '}';
+    }
+}
---
+++ src/chapter07/rabbitTest.java
@@ -0,0 +1,26 @@
+package chapter07;
+
+public class rabbitTest {
+    public static void main(String[] args) {
+        Rabbit rb1 = new Rabbit();
+        Rabbit rb2 = new Rabbit();
+        Rabbit rb3 = new Rabbit();
+
+        System.out.println(rb1);
+        System.out.println(rb2);
+        System.out.println(rb3);
+
+        rb1.setShape("둥글둥글");
+        rb1.setPosition(10,10);
+        rb2.setShape("네모네모");
+        rb2.setPosition(30,30);
+        rb3.setShape("세모세모");
+        rb3.setPosition(100,50);
+
+        System.out.println(rb1);
+        System.out.println(rb2);
+        System.out.println(rb3);
+
+        rb1.move();
+    }
+}
