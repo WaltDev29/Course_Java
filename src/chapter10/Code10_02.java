1acdc57764bb56e631643141323dd761fae4275a
Waltdev29 <waltdev29@gmail.com>
2025-08-27 09:25:01 +0900

    이중 반복문 복습

---
+++ src/chapter10/Code10_02.java
@@ -0,0 +1,27 @@
+package chapter10;
+
+public class Code10_02 {
+    public static void main(String[] args) {
+
+        // 구구단 가로 출력
+//        for (int i=2; i<=9; i++) {
+//            System.out.printf("### %d단 ###\n", i);
+//            for (int j=1; j<=9; j++) {
+//                System.out.printf("%d × %d = %d\t\t", i, j, i*j);
+//            }
+//            System.out.println("\n");
+//        }
+
+        // 가로 출력 심화
+        for (int j = 2; j <= 9; j++) {
+            System.out.printf("### %d단 ###\t\t", j);
+        }
+        System.out.println();
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 9; j++) {
+                System.out.printf("%d × %d = %d\t\t", j, i, j * i);
+            }
+            System.out.println();
+        }
+    }
+}
