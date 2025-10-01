aef1fc5a376ccf871b3c9d2e61baa04795dabd0c
Waltdev29 <waltdev29@gmail.com>
2025-10-01 09:25:19 +0900

    JFrame을 상속받은 클래스를 생성하는 방식 (주석 추가)

--- src/chapter12_JFrame/Code12_01_JframeTest.java
+++ src/chapter12_JFrame/Code12_01_JframeTest.java
@@ -3,7 +3,7 @@ package chapter12_JFrame;
 import javax.swing.*;
 
 public class Code12_01_JframeTest extends JFrame {
-
+    // JFrame을 상속받은 클래스를 생성하는 방식
     public Code12_01_JframeTest() {
         setTitle("처음 만드는 프레임");
         setSize(500,500);
