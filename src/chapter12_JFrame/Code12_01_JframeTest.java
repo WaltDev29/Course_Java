3a5db3536eb098a962d3b454d13d2605188feb7f
Waltdev29 <waltdev29@gmail.com>
2025-10-01 09:27:43 +0900

    JFrame을 상속받은 클래스를 생성하는 방식

--- src/chapter12_JFrame/Code12_01_JframeTest.java
+++ src/chapter12_JFrame/Code12_01_JframeTest.java
@@ -6,6 +6,8 @@ public class Code12_01_JframeTest extends JFrame {
     // JFrame을 상속받은 클래스를 생성하는 방식
     public Code12_01_JframeTest() {
         setTitle("처음 만드는 프레임");
+        // Jframe을 닫으면 프로그램이 종료되는 설정
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500,500);
         setLocation(200,200);
         setVisible(true);
