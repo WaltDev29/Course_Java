fe08ec186997e3bf0277d6a77167a7a63700af13
Waltdev29 <waltdev29@gmail.com>
2025-10-01 09:26:03 +0900

    JFrame 객체를 생성하는 방식

---
+++ src/chapter12_JFrame/Code12_02_JFrameObject.java
@@ -0,0 +1,16 @@
+package chapter12_JFrame;
+
+import javax.swing.*;
+
+public class Code12_02_JFrameObject {
+    public static void main(String[] args) {
+        // JFrame 객체를 생성하는 방식
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("기본 윈도우 창");
+
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+}
